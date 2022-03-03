8512fe3080fdb7ed84fe2ec9c54e0ccbb5e2446c
Triton Halley <Triton.halley@outlook.com>
2022-03-03 18:26:16 +0330

    Separation blog section create a connection between app and database make blog posts dynamic and come from database create search engine and connected it to database add placeholder image

--- var/www/Includes/db.php
+++ var/www/Includes/db.php
@@ -1,3 +1,15 @@
 <?php
 
+$servername = "localhost";
+$username = "padmin";
+$password = "admin";
 
+$connection = mysqli_connect($servername, $username, $password,'cms');
+
+if($connection){
+
+    //echo "<script>alert('connection succesfully')</script>";
+}
+else{
+    echo "<script>alert('connection failed')</script>";
+}
